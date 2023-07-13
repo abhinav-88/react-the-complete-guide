ddef7e206c20ac4a20dd1ffea11263ceeee9029c
abhinav-88 <123355323+abhinav-88@users.noreply.github.com>
2023-07-13 15:07:04 +0530

    React-Router 303.Defining Routes

--- 21-react-router/src/App.js
+++ 21-react-router/src/App.js
@@ -1,5 +1,11 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import HomePage from '../src/pages/Home';
+const mainRouter = createBrowserRouter([
+  { path: '/', element: <HomePage /> },
+  {}
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={mainRouter}/>;
 }
 
 export default App;
